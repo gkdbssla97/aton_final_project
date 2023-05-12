277341358c7ae86ce493029b423b24413b142d3f
Hayoon <hy97@sju.ac.kr>
2023-05-12 18:06:55 +0900

    feat(AccountStatus): 계정 권한 상태. enum Type

--- src/main/java/com/example/aton_final_project/util/constants/AccountStatus.java
+++ src/main/java/com/example/aton_final_project/util/constants/AccountStatus.java
@@ -4,13 +4,17 @@ package com.example.aton_final_project.util.constants;
  * 계정 권한 상태
  */
 public enum AccountStatus {
-    WAIT('N'), APPROVAL('Y'); // 대기 / 승인
-    private final char value;
-    AccountStatus(char value) {
+    NORMAL("0000"), // 일반
+    LONG_TERM_NO_LOGIN("0001"), // 장기미접속자
+    MEMBER_LOCK("0002"), // 계정 잠금
+    MEMBER_PAUSE("0003"), // 계정 중지
+    MEMBER_SECESSION("0004"); // 계정 탈퇴
+    private final String value;
+    AccountStatus(String value) {
         this.value = value;
     }
 
-    public int getValue() {
+    public String getValue() {
         return value;
     }
 }
