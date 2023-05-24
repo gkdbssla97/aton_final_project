28583228e7e67609158c172c59635cf18eee8d3a
Hayoon <hy97@sju.ac.kr>
2023-05-24 10:57:33 +0900

    feat(AuthenticationRegisterError): 중복된 아이디에 따른 예외처리 코드 추가

--- src/main/java/com/example/aton_final_project/util/error/code/AuthenticationRegistrationError.java
+++ src/main/java/com/example/aton_final_project/util/error/code/AuthenticationRegistrationError.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum AuthenticationRegistrationError {
     MISSING_REQUIRED_ITEM(3101, "필수항목 %s이(가) 누락되었습니다.","REGISTER", HttpStatus.BAD_REQUEST),
     INVALID_VALUE(3102, "%s이(가) 유효하지 않습니다.", "REGISTER", HttpStatus.BAD_REQUEST),
+    DUPLICATE_ID(3103, "중복된 아이디 입니다.", "REGISTER", HttpStatus.BAD_REQUEST),
     TEMPORARY_ERROR(3199, "일시적인 오류가 발생했습니다. 잠시 후 다시 요청해주세요.", "REGISTER", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final Integer errorCd;
