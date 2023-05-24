d279f9c266a2cc057a66a0cc69453dfc190adfe3
Hayoon <hy97@sju.ac.kr>
2023-05-24 10:58:10 +0900

    feat(InquiryError): 예외처리 코드 변경

--- src/main/java/com/example/aton_final_project/util/error/code/InquiryError.java
+++ src/main/java/com/example/aton_final_project/util/error/code/InquiryError.java
@@ -7,10 +7,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum InquiryError {
-    MISSING_REQUIRED_ITEM(6101, "필수항목 %s이(가) 누락되었습니다.", "LOGIN", HttpStatus.BAD_REQUEST),
-    INVALID_VALUE(6102, "%s이(가) 유효하지 않습니다.", "LOGIN", HttpStatus.BAD_REQUEST),
+    MISSING_REQUIRED_ITEM(6101, "필수항목 %s이(가) 누락되었습니다.", "INQUIRY", HttpStatus.BAD_REQUEST),
+    INVALID_VALUE(6102, "%s이(가) 유효하지 않습니다.", "INQUIRY", HttpStatus.BAD_REQUEST),
 
-    TEMPORARY_ERROR(6199, "일시적인 오류가 발생했습니다. 잠시 후 다시 요청해주세요.", "LOGIN", HttpStatus.INTERNAL_SERVER_ERROR);
+    TEMPORARY_ERROR(6199, "일시적인 오류가 발생했습니다. 잠시 후 다시 요청해주세요.", "INQUIRY", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final Integer errorCd;
     private final String errorMessage;
